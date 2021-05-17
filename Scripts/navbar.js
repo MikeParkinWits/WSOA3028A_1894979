36081b46da44d1af97b65e80db9d674a577a5cc9
Michael Parkin <1894979@students.wits.ac.za>
2021-05-18 00:12:55 +0200

    Added Basic Reusable Nav Bar

---
+++ Scripts/navbar.js
@@ -0,0 +1,47 @@
+const menuOptions = [
+    {title: 'Portfolio', link: '/WSOA3028A_1894979/PortfolioPage/PortfolioMainPage.html'},
+    {title: 'Blog', link: '/WSOA3028A_1894979/Blogs/AllBlogsP1.html'},
+    {title: 'About Me', link: '/WSOA3028A_1894979/AboutPage/AboutMePage.html'},
+    {title: 'Contact Me', link: '/WSOA3028A_1894979/ContactPage/ContactMePage.html'}
+
+]
+
+const initialiseMenuLogo = (ul) => {
+    const li = document.createElement('li');
+    const link = document.createElement('a');
+    link.href = '/WSOA3028A_1894979/index.html';
+    const image = document.createElement('img');
+    image.src = './Images/Logo.png';
+    image.width = 200;
+    image.alt = 'Jake Sky Logo';
+    link.appendChild(image);
+
+    li.appendChild(link);
+
+    ul.appendChild(li);
+}
+
+const initialiseMenu = () =>{
+    const nav = document.querySelector('.header-nav');
+
+    const ul = document.createElement('ul');
+
+    initialiseMenuLogo(ul);
+
+    for (let option of menuOptions){
+        const li = document.createElement('li');
+        const link = document.createElement('a');
+        link.innerText = option.title;
+        link.href = option.link;
+
+        li.appendChild(link);
+
+        ul.appendChild(li);
+    }
+
+    nav.appendChild(ul);
+};
+
+
+
+document.addEventListener('DOMContentLoaded', () => initialiseMenu());
